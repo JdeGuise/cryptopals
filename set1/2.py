603feaffe8fe4a504e78a35a75a4157c914b0040
John <degui1r@cmich.edu>
2017-11-10 11:43:07 -0700

    Solves set 1 challenge 2

---
+++ set1/2.py
@@ -0,0 +1,9 @@
+def xorStrings(hex_str, xor_val):
+
+	return (int(hex_str, 16) ^ int(xor_val, 16))
+
+
+
+
+if __name__ == '__main__':
+	print hex(xorStrings('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965'))[2:-1]
