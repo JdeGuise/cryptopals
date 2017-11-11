1fe9d2f781f46131c7e3ecde2c120d88922f9d7e
John <degui1r@cmich.edu>
2017-11-11 13:21:39 -0700

    Cleans up exercise 3

--- set1/3.py
+++ set1/3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-def score(text):
+def score(xor_string):
     charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.'\n"
     score = 0
-    for i in text:
+    for i in xor_string:
         if i in charset or i == ' ' or i == '\'':
             score += 1
     return score
 
-# function that performs XOR operation on two strings
+# XOR key against every char in input_str, concat and return result
 def xor(input_str, xor_key):
     result_str = ""
     for i in range(0, len(input_str)):
@@ -16,11 +16,11 @@ def xor(input_str, xor_key):
 
     return result_str
 
-def main(input_str):
+def decodeHex(input_str):
     best_str = ""
     best_score = 0
 
-    # bruteforcing all possible values
+    # bruteforce all possible values
     for i in range(1, 256):
         this_str = xor(input_str.decode("hex"), chr(i))
         if score(this_str) > best_score:
@@ -30,4 +30,4 @@ def main(input_str):
     print "Plaintext: {}".format(best_str)
 
 if __name__ == "__main__":
-    main("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+    decodeHex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
