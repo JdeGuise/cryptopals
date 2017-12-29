fff55d531fd36ce80a72ecf855e51b80c39443d7
John <degui1r@cmich.edu>
2017-12-29 15:13:31 -0700

    Work on 6.py; furthers exercise, breaks up input into KEYSIZE blocks

--- set1/6.py
+++ set1/6.py
@@ -1,14 +1,52 @@
+#!/usr/bin/env python
+from __future__ import division
+from hammingdistance import calculate_hamming_distance
 import binascii
+import sys
 
-def calculateHammingDistance(s1, s2):
-	a = int(bin(int(binascii.hexlify(s1), 16))[2:])
-	b = int(bin(int(binascii.hexlify(s2), 16))[2:])
+# "main"
+def break_repeating_key_xor():
 
-	count = 0
-	for i in range(0, len(str(a))):
-		if str(a)[i] != str(b)[i]:
-			count += 1
-	return count
+	edit_dist_list = dict()
+	chomped_input = ""
+	block_pieces = list()
+	with(open("6.txt", "r")) as input_file:
+		for line in input_file:
+			chomped_input += line
+			for keysize in range(2, 41):
+				# divide hamming distance results by key, then add to list
+				# param 1: initial compared string for hamming distance
+				# param 2: comparison string for hamming distance
+				# params must be of equal length
+				# params should be the first and second KEYSIZE worth of vals
+				# in our input_file line
+				# ?? how can we correlate our appended result with its key?
+				# print "Numbah1: ", line[:keysize]
+				# print "Numbah2: ", line[keysize:(2*keysize)], "\n"
+				#
+				# normalizing result
+				edit_dist_list[keysize] = calculate_hamming_distance(line[:keysize], line[keysize:(2*keysize)])/keysize
+
+			# iterate through keys
+			for key in list(edit_dist_list.keys()):
+				if edit_dist_list[key] == 0.0:
+					del edit_dist_list[key]
+
+		keysize = min(edit_dist_list, key=edit_dist_list.get)
+
+	for i in range(0, len(chomped_input), keysize):
+		block_pieces.append(chomped_input[i:i+keysize])
+
+	# cipher broken up into chunks of size KEYSIZE
+	print block_pieces
+
+
+
 
 if __name__ == '__main__':
-	print calculateHammingDistance("this is a test", "wokka wokka!!!") # => 37
+	assert(calculate_hamming_distance("this is a test".encode("ascii"),
+									"wokka wokka!!!".encode("ascii")) == 37)
+
+	print break_repeating_key_xor()
+
+	# assert(break_repeating_key_xor() == "")
